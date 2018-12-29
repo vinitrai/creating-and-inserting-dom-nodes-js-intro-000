f0143fcee8ce2de2978b08b9ec01d594512bc5af
Vinit Rai <learner.vinit@gmail.com>
2018-12-29 01:47:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
 document.body.appendChild(element);
 element.style.textAlign= 'center';
+
 var ul = document.createElement('ul');
+
 for(let i=0;i<3;i++){
 let li = document.createElement('li');
 li.innerHTML = (i+1).toString();
 ul.appendChild(li);
 }
+
 element.appendChild(ul);
 ul.style.textAlign = 'left';
 ul.removeChild(ul.querySelector('li:nth-child(2)'));
